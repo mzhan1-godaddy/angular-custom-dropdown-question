56107142601db99fa1e14b8b2bd8b2ff63d001a4
Matt Zhan <mzhan1@godaddy.com>
2021-12-17 11:48:44 -0600

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CardComponent} from "../components/card/card.component";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+    change = undefined;
+
+    getChangeValue() {
+        return JSON.stringify(this.change, null, 2);
+    }
 }
--- src/components/card/card.component.ts
+++ src/components/card/card.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
     selector: 'card',
     template: `
         <div class="card">
-            {{title}}
+            <code>{{title}}</code>
             <div class="card-content"><ng-content></ng-content></div>
         </div>
     `,
     styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
-    title = 'sample';
+    @Input() title = 'sample';
 }
